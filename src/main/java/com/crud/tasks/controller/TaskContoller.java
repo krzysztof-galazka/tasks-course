0dabef1320e8b1f0c501d6f307f5fadb14ee6741
krzysztof-galazka <kgalazka@vp.pl>
2020-06-30 18:54:03 +0200

    Task 19.2

--- src/main/java/com/crud/tasks/controller/TaskContoller.java
+++ src/main/java/com/crud/tasks/controller/TaskContoller.java
@@ -1,38 +1,32 @@
 package com.crud.tasks.controller;
 
 import com.crud.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
 public class TaskContoller {
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @RequestMapping (method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks(){
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(Long taskId){
-        return new TaskDto(1L, "test title", "test content");
-    }
-
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(Long taskId){
-
-    }
-
-    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
-    public TaskDto updateTask(TaskDto taskDto){
-        return new TaskDto(1L, "eddited test title", "test content");
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "createTask")
-    public void createTask(TaskDto taskDto){
-
+    public TaskDto getTasks(@RequestParam Long taskId) throws TaskNotFoundException{
+        return taskMapper.mapToTaskDto(service.getTaskById(taskId)
+        .orElseThrow(() -> new TaskNotFoundException("Task doesn't exist")));
     }
 }
